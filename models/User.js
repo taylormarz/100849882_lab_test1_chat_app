71ef9d50cadbf8e5366d3dc4d2afebeafbcc0d1e
Taylor Martin <taylormartind@gmail.com>
2025-02-05 12:01:14 -0500

    updated username to be unique

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-    username:  { type: String, required: true },
+    username:  { type: String, required: true, unique: true },
     firstname: { type: String, required: true },
     lastname:  { type: String, required: true },
     password:  { type: String, required: true },
