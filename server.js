071871404673936bcff5ce211a0dfd56fcd2794f
Taylor Martin <taylormartind@gmail.com>
2025-02-05 11:35:33 -0500

    setup server and db

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const socketio = require('socket.io')
-const app = express()
-const SERVER_PORT = 3000;
+const mongoose = require('mongoose')
 
+const app = express()
+const SERVER_PORT = 3001
+
+app.use(express.json());
+
+// mongodb connection
+const DB_NAME = 'comp3133'
+const DB_USER_NAME = 'taylormarz'
+const DB_PASSWORD = '905Sysco'
+const DB_CONNECTION = `mongodb+srv://${DB_USER_NAME}:${DB_PASSWORD}@cluster0.vb98d.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.connect(DB_CONNECTION)
+.then(() => console.log('Successfully connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
+
+// server connection
 const server = app.listen(SERVER_PORT, () => {
-    console.log('Chat Server running on http://localhost:3000/')
+    console.log(`Server running on http://localhost:${SERVER_PORT}/`)
 })
 
 const io = socketio(server)
@@ -13,6 +28,6 @@ io.on('connection', (socket) => {
     console.log(`New Socket: ${socket.id}`)
 
     socket.on('disconnect', () => {
-        console.log(`User disconnected ${socket.id}`)
+        console.log(`User disconnected: ${socket.id}`)
     })
 })
